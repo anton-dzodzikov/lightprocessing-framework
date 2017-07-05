66797f27b5207957866190d61a460f79617164d8
anton-dzodzikov <anton.dzodzikov@gmail.com>
2017-07-05 23:24:18 +1000

    Define Document interface methods

--- common/src/main/groovy/solutions/lightprocessing/common/documents/Document.groovy
+++ common/src/main/groovy/solutions/lightprocessing/common/documents/Document.groovy
@@ -1,5 +1,16 @@
 package solutions.lightprocessing.common.documents
 
-interface Document {
+import java.time.ZonedDateTime
 
+interface Document {
+    Long getId()
+    Long getParentId()
+
+    ZonedDateTime getCreationDate()
+    ZonedDateTime getLastUpdatedDate()
+
+    String getType()
+    String getName()
+
+    Map<String, Object> getDocumentProperties()
 }
